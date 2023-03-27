4740aa3294907fb7641ab3471b4976a95f952b77
Shruti <agarwalshruti871@gmail.com>
2023-03-27 11:02:31 +0530

    necessary

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title> Student Management System</title>
+    </head>
+    <body> 
+    <label> Govt.Autonomous College </label>
+     <nav class="navbar background">
+       
+        <ul class="nav-list">
+            <li><a href="#Home"Home></a></li>
+            <li><a href="#Contact"Contact></a></li>
+            <li><a href="#Admission"Admission></a></li>
+            <li><a href="#Login"Login></a></li>
+
+        </ul>
+     </nav>
+    </body>
+</html>
